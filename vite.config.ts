5befda0c9e617b3d3b831861d8453522800ea010
Tugdual de Kerdrel <tugdualk@hotmail.com>
2025-02-08 19:22:39 +0000

    fix (vercel): try to fix vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -18,7 +19,17 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'lucide-react': ['lucide-react']
+        }
+      }
+    }
+  }
 });
